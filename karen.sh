924e36f0a112fc904a28c14aa4915dd3a1b5e737
micah <micahjcfull@gmail.com>
2022-01-02 19:05:54 -0800

    ok

--- karen.sh
+++ karen.sh
@@ -6,12 +6,12 @@ echo "karen is triggered $(date +%s)"
 echo "running DOMinate.py"
 # command > file 2>&1
 # python3 ./DOMinate.py > ./pythonerr.log 2>&1
-python3 ./DOMinate.py 2>&1
+python3 ./DOMinate.py 2>&1 # pip stderr to stdout and stdout to file
 echo "running diff"
 echo
 echo
 # or use                                         "^\+"
-diff -u scraped.csv scraped_latest.csv | grep -E "^\-"
+diff -u scraped.csv scraped_latest.csv | grep -E "^\+"
 echo
 echo
 echo "karen is done $(date +%s)"
